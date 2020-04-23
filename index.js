330a7ce3fa0816a87fff096c28342a67af10b968
winnyiee@naver.com <winnyiee@naver.com>
2020-04-23 18:01:42 +0900

    first

---
+++ index.js
@@ -0,0 +1,17 @@
+
+
+/*document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById("autofill").addEventListener("click", autoFill);
+});
+function autoFill() {
+    document.getElementById('input1').value = "My Text Input";
+    document.getElementById('input2').value = "Dropdown2";
+
+    var radioElements = document.getElementsByName("input3");
+
+    for (var i=0; i<radioElements.length; i++) {
+        if (radioElements[i].getAttribute('value') == 'Radio3') {
+            radioElements[i].checked = true;
+        }
+    }
+}*/
