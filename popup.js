ff641faa3f51e23fe4246680bf8b88ade4545408
winnyiee@naver.com <winnyiee@naver.com>
2020-04-23 23:06:07 +0900

    add birth

--- popup.js
+++ popup.js
@@ -1,26 +1,23 @@
 const func = () => {
 
-    chrome.storage.sync.get(['engName'], function(result) {
-        console.log('Value currently is ' + result.engName);
-    });
-
     chrome.tabs.executeScript({file:"test.js"})
 
 
 }
 
-const name = () =>{
-    const {value : englishName} = engName
-    console.log(englishName)
-    chrome.storage.sync.set({'engName': englishName}, function() {
-        console.log('Value is set to ' + englishName);
+const save = () =>{
+    const {value : englishName} = inputEngName
+    const {value : birthday} = inputBirth
+    chrome.storage.sync.set({'engName': englishName,'birth':birthday}, function() {
+        console.log('Value is set to ' + engName);
     });
 }
 
 const btn1 = document.getElementById('btn1');
 const btn2 = document.getElementById('btn2');
-const engName = document.getElementById('engName');
+const inputEngName = document.getElementById('engName');
+const inputBirth = document.getElementById('birthday');
 btn1.addEventListener("click",func);
-btn2.addEventListener("click",name);
+btn2.addEventListener("click",save);
 
 
--- test.js
+++ test.js
@@ -1,5 +1,6 @@
 
-chrome.storage.sync.get(['engName'], function(result) {
+chrome.storage.sync.get(['engName','birth'], function(result) {
     console.log('Value currently is ' + result.engName);
     document.querySelector('#englishName').value=result.engName;
+    document.querySelector('#birthday').value=result.birth;
 });
