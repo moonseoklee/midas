d024eca764ed7a058f06c91100d61f53edba6fdc
winnyiee@naver.com <winnyiee@naver.com>
2020-04-24 14:50:25 +0900

    200424

--- popup.js
+++ popup.js
@@ -6,17 +6,29 @@ const func = () => {
 }
 
 const save = () =>{
-    const {value : englishName} = inputEngName
-    const {value : birthday} = inputBirth
-    chrome.storage.sync.set({'engName': englishName,'birth':birthday}, function() {
-        console.log('Value is set to ' + engName);
+
+
+    const englishName = inputLastname.value+" "+inputFirstname.value
+
+    const birthday = inputBirth.value
+
+    const {value : gender} = inputGender
+    const {value : hobby} = inputHobby
+    const {value : specialty} = inputSpecialty
+    chrome.storage.sync.set({'engName': englishName,'birthday':birthday,'gender':gender,'hobby':hobby,'specialty':specialty}, function() {
+
     });
 }
 
 const btn1 = document.getElementById('btn1');
 const btn2 = document.getElementById('btn2');
-const inputEngName = document.getElementById('engName');
+const inputLastname = document.getElementById('lastname');
+const inputFirstname = document.getElementById('firstname');
 const inputBirth = document.getElementById('birthday');
+const inputGender = document.getElementById('gender');
+const inputHobby = document.getElementById('hobby');
+const inputSpecialty = document.getElementById('specialty');
+const fullEngName = inputLastname+" "+inputFirstname;
 btn1.addEventListener("click",func);
 btn2.addEventListener("click",save);
 
--- test.js
+++ test.js
@@ -1,6 +1,8 @@
 
-chrome.storage.sync.get(['engName','birth'], function(result) {
+chrome.storage.sync.get(['engName','birthday','hobby','specialty'], function(result) {
     console.log('Value currently is ' + result.engName);
     document.querySelector('#englishName').value=result.engName;
-    document.querySelector('#birthday').value=result.birth;
+    document.querySelector('#birthday').value=result.birthday;
+    document.getElementsByName('#hobby').value=result.hobby;
+    document.getElementsByName('#specialAbility').value=result.specialty;
 });
