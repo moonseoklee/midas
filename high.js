3104d50b8a74aa6566c424473fc47d945e3f8c4a
winnyiee@naver.com <winnyiee@naver.com>
2020-04-30 21:56:11 +0900

    add exception

--- high.js
+++ high.js
@@ -4,10 +4,13 @@ chrome.storage.local.get(['firstName','lastName','birthday','hobby','specialty',
     var e = $.Event("keypress");
     e.which = 13;
     e.keyCode = 13;
-    document.querySelectorAll('[data-type="highschool"]')[0].value="가수";
-
+    document.querySelectorAll('[data-type="highschool"]')[0].value="정발";
+    console.log(e)
     $(document.querySelectorAll('[data-type="highschool"]')[0]).trigger(e);
-    console.log("trig");
+
+
+
+
 
 
 });
--- storage.js
+++ storage.js
@@ -1,19 +1,26 @@
 
 chrome.storage.local.get(['firstName','lastName','birthday','hobby','specialty','gender','army','armyWhere','armyPosition','armyStart','armyEnd','armyDischarge'], function(result) {
 
-    document.querySelector('#englishName').value=result.lastName+" "+result.firstName;
+    if(document.querySelector('#englishName')!=null){
+        document.querySelector('#englishName').value=result.lastName+" "+result.firstName;
+    }
     document.querySelector('#birthday').value=result.birthday.slice(0,4)+"."+result.birthday.slice(4,6)+"."+result.birthday.slice(6,8);
-    document.querySelectorAll('[name="hobby"]')[0].value=result.hobby;
-    document.querySelectorAll('[name="specialAbility"]')[0].value=result.specialty;
-
-    if(result.gender=='남'){
-        document.querySelectorAll('[name="genderFlag"]')[0].checked=true;
-    }else{
-        document.querySelectorAll('[name="genderFlag"]')[1].checked=true;
+    if(document.querySelectorAll('[name="hobby"]')[0]!=null){
+    document.querySelectorAll('[name="hobby"]')[0].value=result.hobby;}
+    if( document.querySelectorAll('[name="specialAbility"]')[0]!=null){
+    document.querySelectorAll('[name="specialAbility"]')[0].value=result.specialty;}
+    if(document.querySelectorAll('[name="genderFlag"]')[0]!=null) {
+        if (result.gender == '남') {
+            document.querySelectorAll('[name="genderFlag"]')[0].checked = true;
+        } else {
+            document.querySelectorAll('[name="genderFlag"]')[1].checked = true;
+        }
     }
     document.querySelectorAll('[name="military.militaryTypeCode"]')[result.army].checked=true;
-    document.getElementById('militaryBranchCode').value=result.armyWhere;
-    document.querySelectorAll('[name="military.militaryPositionCode"]')[0].value=result.armyPosition;
+    if(document.getElementById('militaryBranchCode')!=null){
+    document.getElementById('militaryBranchCode').value=result.armyWhere;}
+    if( document.querySelectorAll('[name="military.militaryPositionCode"]')[0]!=null){
+    document.querySelectorAll('[name="military.militaryPositionCode"]')[0].value=result.armyPosition;}
     document.querySelectorAll('[name="military.militaryStartDate"]')[0].value=result.armyStart.slice(0,4)+"."+result.armyStart.slice(4,6)+"."+result.armyStart.slice(6,8);
     document.querySelectorAll('[name="military.militaryEndDate"]')[0].value=result.armyEnd.slice(0,4)+"."+result.armyEnd.slice(4,6)+"."+result.armyEnd.slice(6,8);
     document.querySelectorAll('[name="military.militaryDischargeCode"]')[0].value=result.armyDischarge;
