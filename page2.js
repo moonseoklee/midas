9047cffb75a0eb00d0ccb524dc70369a0d7ecc28
winnyiee@naver.com <winnyiee@naver.com>
2020-05-15 20:15:10 +0900

    add school err

--- page2.js
+++ page2.js
@@ -4,17 +4,19 @@ chrome.storage.local.get(['universityEntranceType','universityStart','university
     let universityName = result.university;
     let t = []
     let t2 = []
+
     t.push(`<span>${highSchoolName}</span>`);
     t.push('<button type="button" class="resetSearchResult" data-button="resetSearchResult"></button>');
 
     t2.push(`<span>${universityName}</span>`);
     t2.push('<button type="button" class="resetSearchResult" data-button="resetSearchResult"></button>');
-
+    //////////////////////// html에 가시되지만 제대로 작동하지 않는다.
     if($(document).find(`input[name$="highschool.academyName"]`)!=null){
     $(document).find(`input[name$="highschool.academyName"]`).val(universityName).next('span.searchResultName').html(t.join(''));}
     if($(document).find(`input[name$="college[0].academyName"]`)!=null){
         $(document).find(`input[name$="college[0].academyName"]`).val(universityName).next('span.searchResultName').html(t2.join(''));}
 
+    //고등학교
     if(document.querySelectorAll('[name="highschool.graduationTypeCode"]')[(result.highSchoolGraduate)]!=null){
     document.querySelectorAll('[name="highschool.graduationTypeCode"]')[(result.highSchoolGraduate)].checked=true;}
     if(document.querySelectorAll('[name="highschool.locationCode"]')[0]!=null){
@@ -29,7 +31,7 @@ chrome.storage.local.get(['universityEntranceType','universityStart','university
     document.querySelectorAll('[name="highschool.graduationDate"]')[0].value=result.highSchoolEnd.slice(0,4)+"."+result.highSchoolEnd.slice(4,6);}
 
 
-
+    //대학교
     if(document.querySelectorAll('[name="college[0].degreeTypeCode"]')[(result.univDegreeType)]!=null){
         document.querySelectorAll('[name="college[0].degreeTypeCode"]')[(result.univDegreeType)].checked=true;}
     if(document.querySelectorAll('[name="college[0].locationCode"]')[0]!=null){
